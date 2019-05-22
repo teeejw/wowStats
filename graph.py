4e6b95b40ff908e82b7063b9af31bb9a64e47c08
teeejw <tjwebb@comcast.net>
2019-05-22 16:14:10 -0700

    worked on graph representation

--- graph.py
+++ graph.py
@@ -5,46 +5,75 @@ import sqlite3
 conn = sqlite3.connect('players.db')
 cur = conn.cursor()
 
-query = '''SELECT crit, haste, mastery, versatility FROM players order by haste'''
+query = '''SELECT rank, crit, haste, mastery, versatility FROM players order by haste'''
 cur.execute(query)
 data = cur.fetchall()
 
+rank_values = []
 crit_values = []
 haste_values = []
 mastery_values = []
 versatility_values = []
 
 for row in data:
-	crit_values.append(row[0])
-	haste_values.append(row[1])
-	mastery_values.append(row[2])
-	versatility_values.append(row[3])
+	rank_values.append(row[0])
+	crit_values.append(row[1])
+	haste_values.append(row[2])
+	mastery_values.append(row[3])
+	versatility_values.append(row[4])
 
 def normalize(values):
 	total = 0
 	for val in values:
 		total += val
 	avg = total/len(values)
-	print(avg)
+
 	weighted_values = []
 	for val in values:
 		weighted_values.append(val/avg)
-
-	print(values)
-	print(weighted_values)
 	return weighted_values
+'''
 
+look at rank 1-4 top->bottom
+
+categorize by which is top?
+map
+	c1 h2 m3 v4
+	c1 h2 v3 m4
+	.
+	.
+	v1 m2 c3 h4
+	v1 m2 h3 c4
+
+
+
+crit_values = sorted(normalize(crit_values))
+haste_values = sorted(normalize(haste_values))
+mastery_values = sorted(normalize(mastery_values))
+versatility_values = sorted(normalize(versatility_values))
+
+'''
 crit_values = normalize(crit_values)
 haste_values = normalize(haste_values)
 mastery_values = normalize(mastery_values)
 versatility_values = normalize(versatility_values)
 
-plt.plot(crit_values, haste_values, color='r',linestyle="",marker="o")
-plt.plot(crit_values, mastery_values, color='b',linestyle="",marker="o")
-plt.plot(crit_values, versatility_values, color='g',linestyle="",marker="o")
+plt.plot(rank_values, crit_values, color='k', linestyle="", marker="o")
+plt.plot(rank_values, haste_values, color='r', linestyle="", marker="o")
+plt.plot(rank_values, mastery_values, color='m', linestyle="", marker="o")
+plt.plot(rank_values, versatility_values, color='g', linestyle="", marker="o")
+'''
 
-plt.title('haste, mastery, versatility vs critical strike')
-plt.legend(['haste','mastery','versatility'])
+plt.plot(crit_values, rank_values, color='k', linestyle="", marker="o")
+plt.plot(haste_values, rank_values, color='r', linestyle="", marker="o")
+plt.plot(mastery_values, rank_values, color='m', linestyle="", marker="o")
+plt.plot(versatility_values, rank_values, color='g', linestyle="", marker="o")
+'''
+
+plt.title('critical strike, haste, mastery, versatility by rank')
+plt.legend(['critical strike', 'haste','mastery','versatility'])
+plt.xlabel('Player Rank')
+plt.ylabel('normalized stat values')
 
 plt.show()
 
